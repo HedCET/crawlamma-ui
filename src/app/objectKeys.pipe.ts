f260d816e2471cf6535e210d60953c8780c651d0
Linto Cheeran <linto.cet@gmail.com>
2020-08-19 18:27:38 +0530

    removed shuffle from client side

--- src/app/objectKeys.pipe.ts
+++ src/app/objectKeys.pipe.ts
@@ -1,9 +1,8 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { shuffle } from "lodash";
 
 @Pipe({ name: "objectKeys", pure: false })
 export class ObjectKeys implements PipeTransform {
   transform(value: any, args: any[] = null): any {
-    return shuffle(Object.keys(value));
+    return Object.keys(value);
   }
 }
